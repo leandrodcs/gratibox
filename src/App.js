f46810c4133a6b4478481a38f7e92cbc5ae6cde6
leandrodcs <eng.leandrodcs@gmail.com>
2021-11-18 19:17:08 -0300

    Setting up font and sign pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./assets/reset.css";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Welcome from "./pages/Welcome";
 
 export default function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact >{process.env.REACT_APP_API_URL === "prod" ? "https://gratibox-back.herokuapp.com" : "http://localhost:4000"}</Route>
+                <Route path="/" exact render={() => <Welcome />}/>
                 <Route path="/sign-up" exact />
                 <Route path="/sign-in" exact />
                 <Route path="*">
