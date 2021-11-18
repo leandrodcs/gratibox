b044cc719191cb837d4247742a6f4584ce04f830
leandrodcs <eng.leandrodcs@gmail.com>
2021-11-18 20:52:46 -0300

    Welcome page relocation added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,15 +1,21 @@
+import { useHistory } from 'react-router';
 import styled from 'styled-components';
 import image from '../assets/image05.webp';
 
 export default function Welcome() {
+    const history = useHistory();
+
+    function relocate(whereTo) {
+        history.push(whereTo);
+    }
     return (
         <PageStyle>
             <Greetings>Bem vindo ao GratiBox</Greetings>
             <Description>Receba em casa um box com chás, produtos organicos, incensos e muito mais...</Description>
             <Image src={image} alt='Welcome'/>
             <Filler>
-                <Button background='#8C97EA'>Quero começar</Button>
-                <Button background='inherit'>Já sou grato</Button>
+                <Button onClick={() => relocate('/sign-up')} background='#8C97EA'>Quero começar</Button>
+                <Button onClick={() => relocate('/sign-in')} background='inherit'>Já sou grato</Button>
             </Filler>
         </PageStyle>
     );
